302f21529223a873912f4b8b89de39808efbbf29
Michele Gherlone <michele.gherlone@liceoberchet.gov.it>
2018-12-29 00:32:09 +0100

    Initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RicercaComponent } from './ricerca/ricerca.component';
+import { InserimentoComponent } from './inserimento/inserimento.component';
+import { CanActivateGuard } from './can-activate.guard';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
+  { path: 'cerca', component: RicercaComponent, canActivate: [CanActivateGuard] },
+  { path: 'inserisci', component: InserimentoComponent, canActivate: [CanActivateGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { RicercaComponent } from './ricerca/ricerca.component';
+import { InserimentoComponent } from './inserimento/inserimento.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { LibrodetailComponent } from './librodetail/librodetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    RicercaComponent,
+    InserimentoComponent,
+    LibrodetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModalModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [LibrodetailComponent]
+
 })
 export class AppModule { }
