20667ed1dea22a111c9570541a0c145d02cc8ea4
Michele Gherlone <michele.gherlone@liceoberchet.gov.it>
2018-12-30 01:55:14 +0100

    Modal dialogue added

---
+++ src/app/confirmation-dialog/confirmation-dialog.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { ConfirmationDialogComponent } from './confirmation-dialog.component';
+
+@Injectable()
+export class ConfirmationDialogService {
+    
+    constructor(private modalService: NgbModal) { }
+    
+    public confirm(title: string, message: string, btnOkText: string = 'OK', btnCancelText: string = 'Annulla', dialogSize: 'sm'|'lg' = 'sm'): Promise<any> {
+            const modalRef = this.modalService.open(ConfirmationDialogComponent, { size: dialogSize });
+            modalRef.componentInstance.title = title;
+            modalRef.componentInstance.message = message;
+            modalRef.componentInstance.btnOkText = btnOkText;
+            modalRef.componentInstance.btnCancelText = btnCancelText;
+            return modalRef.result;
+        }        
+}
